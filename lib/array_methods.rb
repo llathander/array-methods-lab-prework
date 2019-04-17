2489fcd4094f5b329484aca59fb477eb0fcccd19
Adrian Samaniego <llathander@gmail.com>
2019-04-17 03:23:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,16 @@
 def using_include(array, element)
+  array.include?(element)
 
 end
 
 def using_sort(array)
-
+  array = ["I", "wow", "I"]
+  return array.sort
 end
 
 def using_reverse(array)
-
+  array = ["arrays!", "arrays!", "wow", "arrays!"]
+  return array.reverse
 end
 
 def using_first(array)
